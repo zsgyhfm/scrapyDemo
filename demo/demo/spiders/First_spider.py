f360d54e68caa8fc1bf8fcfcc1ff705a08048be9
xiaoyu <xkas@gmail.com>
2019-04-11 15:08:10 +0800

    1. 将详情中的图片地址保存下来 2. 注意:因为每次请求url都会调用一个parse函数，导致写入时候必须选择w+ 不能选择追加 否则内容重复

--- demo/demo/spiders/First_spider.py
+++ demo/demo/spiders/First_spider.py
@@ -1,11 +1,13 @@
 import scrapy
 import json
+from scrapy.http.response import Response
 
 
 class FirstSpider(scrapy.Spider):
     name = "First_spider"
     # start_urls = ['http://lab.scrapyd.cn']  等同于下面的start_requests方法
     data = []
+    src_data = []
 
     def start_requests(self):
         urls = [
@@ -14,7 +16,7 @@ class FirstSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse(self, response):
+    def parse(self, response: Response):
         # 提取首页所有名言，保存至变量mingyan
         mingyan = response.css('div.quote')
         # 下一页
@@ -26,6 +28,8 @@ class FirstSpider(scrapy.Spider):
             json_data['tags'] = item.css('.tags .tag::text').extract()  # 提取标签
             json_data["details"] = item.css("span>a::attr(href)").extract_first()  # 提取详情地址
             self.data.append(json_data)
+            # 获取图片地址
+            yield response.follow(json_data["details"], callback=self.parse_src)
 
         if next_page is not None:
             print("*" * 50)
@@ -35,5 +39,22 @@ class FirstSpider(scrapy.Spider):
             yield response.follow(next_page, callback=self.parse)
         # 注意 windows下 文件编码是GBK  这里要指定打开编码
         with open("data.json", "w+", encoding='utf-8') as f:
+            print("-" * 50)
+            print("保存------地址")
+            print("-" * 50)
             s = json.dumps(self.data, ensure_ascii=False)
             f.write(s)
+        with open("src.json", "w+", encoding="utf-8") as f:
+            print("+"*50)
+            print("保存src地址")
+            print("+" * 50)
+            f.write(json.dumps(self.src_data, ensure_ascii=False))
+
+        # 上面的数据保存完毕后 读取json_data 下载图片
+
+    """讲详情里面的图片下载下来"""
+
+    def parse_src(self, response: Response):
+        content = response.xpath('//div[@class="post-content"]//img/@src').extract()
+        for i in content:
+            self.src_data.append({"src": i})
